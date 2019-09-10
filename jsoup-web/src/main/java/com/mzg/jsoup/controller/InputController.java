d7d1e67f4471c9b456e323ec84fa626a28c22683
mazhigang <mazhigang@dianxiaohuo.net>
2019-09-10 10:54:52 +0800

    搭建工程

---
+++ jsoup-web/src/main/java/com/mzg/jsoup/controller/InputController.java
@@ -0,0 +1,15 @@
+package com.mzg.jsoup.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: 马治刚
+ * @Mail mazhigang@dianxiaohuo.net
+ * @Date: 2019/9/10 10:53
+ * @Description
+ */
+@RestController
+public class InputController {
+
+}
